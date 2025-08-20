948bf4f318f7cdaa3634a45942426523760ddf22
mengyihao666 <yihaomeng6@gmail.com>
2025-08-20 15:27:46 +0800

    print输出字符串

---
+++ 2222.py
@@ -0,0 +1,10 @@
+name = '张三'
+age = 28
+height = 1.83
+school = '阳明学院'
+boys = 36
+girls = 12
+total = boys + girls
+# +
+print("我叫"+name+"，我的年龄是"+str(age)+"岁，身高"+str(height)+"，就读于"+school +
+      "，我们班男同学人数是"+str(boys)+"，女同学人数是"+str(girls)+"，总共有"+str(total)+"人。")
